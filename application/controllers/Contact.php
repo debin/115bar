1ca388c6d2a26d99a67fffc5b50295c7eb3b272e
liangdebin <touch767@gmail.com>
2015-04-26 19:11:18 +0800

    115app

---
+++ application/controllers/Contact.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * 联系方式
+ * @author ldb
+ * @date(2015-04-26)
+ */
+
+class ContactController extends BasicController {
+
+    /**
+     * 搜索
+     *
+     * @author ldb
+     * @date(2014-12-15)
+     */
+    public function indexAction() {
+        // $key = $this->getRequest()->getQuery("key",'');
+        $key = $this->getRequest()->getParam("key", '');
+        $key = urldecode($key);
+        $page = $this->getRequest()->getParam("page", 1);
+        $page = intval($page);
+        if (empty($page)) {
+            $page = 1;
+        }
+        $pagesize = 20;
+        $res = SearchModel::getZoneInfo($key,$page,$pagesize);
+        $list_data = $res['list_data'];
+        $total = $res['total'];
+        if (empty($key)) {
+            $total = $pagesize;
+        }
+        // 分页
+        $url = Yaf_Registry::get("config")->webroot . "/s/$key";
+        // echo json_encode( get_defined_vars() );
+        // $SERVER = $this->getRequest()->getServer();
+        $getpage = new PageModel($url,$total, $page,$pagesize,'/');
+        $paginate = $getpage->showpage();
+        // echo $paginate;
+        // exit;
+
+        $this->title = _("la_103")."_"._("la_102");
+        $output                       = array();
+        $output['total']              = $total;
+        $output['list']               = $list_data;
+        $output['data']['page']       = $page;
+        $output['data']['pagesize']   = $pagesize;
+        $output['data']['page_count'] = 0;
+        $output['paginate']           = $paginate;
+        $output['key']           = $key;
+        // $output                       = cat_html($output);
+        $this->getView()->assign("output", $output);
+        // $this->getView()->display("topics/index.html");
+    }
+
+}
