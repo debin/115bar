bdd7f58bb5ce6e3ce01755008c88ecd51de9a3f3
liangdebin <touch767@gmail.com>
2015-06-22 00:00:47 +0800

    topic sync

--- application/shell/topic_sync_delta.php
+++ application/shell/topic_sync_delta.php
@@ -26,46 +26,56 @@ $db_new = new PgsqlHelper();
 $servers = ConfigPg::$config['ol'][$dbname];
 $db_new ->connect($servers[0],$servers[1],$servers[2],$dbname,$servers[3]);
 
-$sql = "SELECT  COUNT(*) AS count FROM ".Otable::TABLE_115_TOPIC;
-$vars = array();
+// update_time
+$sql = 'SELECT * FROM "update_index" WHERE upload_id=? AND "type"=? ;';
+$vars = array('ol',$index_type);
+$xun_index = $db_new->getOne($sql,$vars);
+if (isset($xun_index['update_time'])) {
+    $update_time = (int)$xun_index['update_time'];
+}else{
+    echo 'no update_time';
+    exit();
+}
+
+
+$sql = "SELECT  COUNT(*) AS count FROM ".Otable::TABLE_115_TOPIC . ' WHERE update_time>?';
+$vars = array($update_time);
 $result = $db_old->getOne($sql,$vars);
 $total = 0;
 if (isset($result['count'])) {
     $total = intval($result['count']);
 }
 $page_count = ($total<=$pagesize)?1:intval(ceil($total/$pagesize));
-
+// var_dump($total);exit;
 // $page_count = 1;
 $count = 0;
 for ($page=1; $page <= $page_count; $page++) {
 
     $offset   = ($page-1)*$pagesize;
-    $sql = 'SELECT "id","subject","abstract","post_time","deal_content","update_time","tags","status" FROM '.Otable::TABLE_115_TOPIC . ' ORDER BY "update_time" ASC,"id" ASC LIMIT ? OFFSET ? ';
-    $vars = array($pagesize,$offset);
+    $sql = 'SELECT "id","subject","abstract","post_time","deal_content","update_time","tags","status" FROM '.Otable::TABLE_115_TOPIC . ' WHERE update_time>? ORDER BY "update_time" ASC,"id" ASC LIMIT ? OFFSET ? ';
+    $vars = array($update_time,$pagesize,$offset);
     $list_arr = $db_old->getAll($sql,$vars);
+
     if ($list_arr) {
         foreach ($list_arr as $key => $value) {
-            $db_new->insert(Otable::TABLE_115_TOPIC,$value);
+            $sql_tmp = "SELECT  id FROM ".Otable::TABLE_115_TOPIC . ' WHERE id=?';
+            $vars_tmp = array($value['id']);
+            $has_id = $db_new->getOne($sql_tmp,$vars_tmp);
+            if ($has_id) {
+                $conditon = array('id'=>$value['id']);
+                $db->update(Otable::TABLE_115_TOPIC,$value,$conditon);
+            }else{
+                $db_new->insert(Otable::TABLE_115_TOPIC,$value);
+            }
             $count++;
         }
     }
 }
 // 更新索引
-$sql = 'SELECT * FROM "update_index" WHERE upload_id=? AND "type"=? ;';
-$vars = array(CONFIG_ENV,$index_type);
-$xun_index = $db_new->getOne($sql,$vars);
-if ($xun_index) {
+if (isset($value['update_time'])&&$value['update_time']) {
     $conditon = array('upload_id'=>CONFIG_ENV,'type'=>$index_type);
-    $update_data = array('update_time'=>$update_time);
-    $db_new->update("update_index",$update_data,$conditon);
-}else{
-    $insert_data = array(
-        'upload_id'   =>CONFIG_ENV,
-        'type'        =>$index_type,
-        'create_time' =>$update_time,
-        'update_time' =>$update_time,
-        );
-    $db_new->insert("update_index",$insert_data);
+    $update_data = array('update_time'=>$value['update_time']);
+    $db->update("update_index",$update_data,$conditon);
 }
 
-echo "update topic sync  total:",$count;
+echo "update topic sync delta total:",$count;
