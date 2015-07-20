39c593af9064f04c520b8dfcc068b0d3c7825779
liangdebin <touch767@gmail.com>
2015-07-20 10:44:16 +0800

    plugin style

--- application/plugins/Layout.php
+++ application/plugins/Layout.php
@@ -1,28 +1,39 @@
 <?php
-class LayoutPlugin extends Yaf_Plugin_Abstract {
+
+/**
+ * Ap定义了如下的7个Hook,
+ * 插件之间的执行顺序是先进先Call
+ */
+class LayoutPlugin extends Yaf_Plugin_Abstract
+{
 
     private $_layoutDir;
     private $_layoutFile;
     private $_layoutVars =array();
 
-    public function __construct($layoutDir=null){
+    public function __construct($layoutDir = null)
+    {
         $this->_layoutFile = '';
         $this->_layoutDir = ($layoutDir) ? $layoutDir : APPLICATION_PATH.'/views/layout/';
     }
 
-    public function  __set($name, $value) {
+    public function  __set($name, $value)
+    {
         $this->_layoutVars[$name] = $value;
     }
 
-    public function dispatchLoopShutdown ( Yaf_Request_Abstract $request , Yaf_Response_Abstract $response ){
+    public function dispatchLoopShutdown(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response)
+    {
 
     }
 
-    public function dispatchLoopStartup ( Yaf_Request_Abstract $request , Yaf_Response_Abstract $response ){
+    public function dispatchLoopStartup(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response)
+    {
 
     }
 
-    public function postDispatch ( Yaf_Request_Abstract $request , Yaf_Response_Abstract $response ){
+    public function postDispatch(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response)
+    {
         /* get the body of the response */
         $body = $response->getBody();
 
@@ -50,19 +61,23 @@ class LayoutPlugin extends Yaf_Plugin_Abstract {
         $response->setBody($layout->render($this->_layoutFile));
     }
 
-    public function preDispatch ( Yaf_Request_Abstract $request , Yaf_Response_Abstract $response ){
+    public function preDispatch(Yaf_Request_Abstract $request , Yaf_Response_Abstract $response)
+    {
 
     }
 
-    public function preResponse ( Yaf_Request_Abstract $request , Yaf_Response_Abstract $response ){
+    public function preResponse(Yaf_Request_Abstract $request , Yaf_Response_Abstract $response)
+    {
 
     }
 
-    public function routerShutdown ( Yaf_Request_Abstract $request , Yaf_Response_Abstract $response ){
+    public function routerShutdown(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response)
+    {
 
     }
 
-    public function routerStartup ( Yaf_Request_Abstract $request , Yaf_Response_Abstract $response ){
+    public function routerStartup(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response)
+    {
 
     }
 }
--- application/plugins/User.php
+++ application/plugins/User.php
@@ -4,9 +4,11 @@
  * Ap定义了如下的7个Hook,
  * 插件之间的执行顺序是先进先Call
  */
-class UserPlugin extends Yaf_Plugin_Abstract {
+class UserPlugin extends Yaf_Plugin_Abstract
+{
 
-    public function routerStartup(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response) {
+    public function routerStartup(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response)
+    {
         // 防止get post注入
         $_POST = cat_escape($_POST);
         $_GET  = cat_escape($_GET);
@@ -17,12 +19,13 @@ class UserPlugin extends Yaf_Plugin_Abstract {
      * @author dbb
      * @data 2014/12/26
      */
-    public function routerShutdown(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response) {
+    public function routerShutdown(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response)
+    {
         $controller = strtolower($request->getControllerName());
         $action = strtolower($request->getActionName());
 
         // 登陆页之外的处理
-        if($controller != "sign"){
+        if ($controller != "sign") {
 
             //项目开始，未登录跳转到登陆页面
             if (!Yaf_Session::getInstance()->user) {
@@ -31,26 +34,31 @@ class UserPlugin extends Yaf_Plugin_Abstract {
         }
     }
 
-    public function dispatchLoopStartup(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response) {
+    public function dispatchLoopStartup(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response)
+    {
 
     }
 
-    public function preDispatch(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response) {
+    public function preDispatch(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response)
+    {
 //        $id = $_REQUEST["id"];
 //        $controller = strtolower($request->getControllerName());
 //        $action = strtolower($request->getActionName());
 //        User_LogModel::addLog($controller, $action, "123".$id);
     }
 
-    public function postDispatch(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response) {
+    public function postDispatch(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response)
+    {
 
     }
 
-    public function dispatchLoopShutdown(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response) {
+    public function dispatchLoopShutdown(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response)
+    {
 
     }
 
-    public function preResponse(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response) {
+    public function preResponse(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response)
+    {
 
     }
 
