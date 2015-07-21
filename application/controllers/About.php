b2052ce888848dedd36b5804bfabf712eb0c5ab8
liangdebin <touch767@gmail.com>
2015-07-21 15:25:35 +0800

    abount

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -17,38 +17,11 @@ class AboutController extends BasicController
     public function indexAction()
     {
         // $key = $this->getRequest()->getQuery("key",'');
-        $key = $this->getRequest()->getParam("key", '');
-        $key = urldecode($key);
-        $page = $this->getRequest()->getParam("page", 1);
-        $page = intval($page);
-        if (empty($page)) {
-            $page = 1;
-        }
-        $pagesize = 20;
-        $res = SearchModel::getZoneInfo($key, $page, $pagesize);
-        $list_data = $res['list_data'];
-        $total = $res['total'];
-        if (empty($key)) {
-            $total = $pagesize;
-        }
-        // 分页
-        $url = Yaf_Registry::get("config")->webroot . "/s/$key";
-        // echo json_encode( get_defined_vars() );
-        // $SERVER = $this->getRequest()->getServer();
-        $getpage = new PageModel($url, $total, $page, $pagesize,'/');
-        $paginate = $getpage->showpage();
         // echo $paginate;
         // exit;
 
         $this->title = _("la_102")." › " ._("la_105");
         $output                       = array();
-        $output['total']              = $total;
-        $output['list']               = $list_data;
-        $output['data']['page']       = $page;
-        $output['data']['pagesize']   = $pagesize;
-        $output['data']['page_count'] = 0;
-        $output['paginate']           = $paginate;
-        $output['key']                = $key;
         // $output                       = cat_html($output);
         $this->getView()->assign("output", $output);
         // $this->getView()->display("topics/index.html");
